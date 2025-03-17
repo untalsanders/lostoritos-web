0dac6f574ac686c0104eac735cf2dccc80fbf066
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-03-17 20:25:48 -0300

    chore: responsive step-1
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/app/logo/logo.component.ts
+++ src/app/logo/logo.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core'
+import { LogoSvgComponent } from '../logo-svg/logo-svg.component'
+import { NgOptimizedImage } from '@angular/common'
 
 @Component({
     selector: 'app-logo',
     standalone: true,
     templateUrl: './logo.component.html',
     styleUrl: './logo.component.css',
+    imports: [LogoSvgComponent, NgOptimizedImage],
 })
 export class LogoComponent {}
