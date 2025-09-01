1c2fe0da5e2efcbe6e0adebaa18c402e248d06e3
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-09-01 08:31:53 -0300

    fix: add admin section
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

---
+++ src/app/features/dashboard/presentation/components/admin-dashboard/admin-dashboard.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-admin-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-dashboard.html',
+  styleUrl: './admin-dashboard.css',
+})
+export class AdminDashboard {}
