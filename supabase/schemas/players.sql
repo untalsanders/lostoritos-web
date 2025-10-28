6e36624f792d335a6bb8b48392f49707d1c9cc90
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-10-28 19:44:04 -0300

    chore(supabase): update seed paths to glob and format players table

--- supabase/schemas/players.sql
+++ supabase/schemas/players.sql
@@ -1,10 +1,11 @@
-create table players (
-  id integer not null primary key,
-  first_name character(30) not null unique,
-  last_name character(30),
-  birthdate date not null,
-  height smallint not null,
-  weight smallint not null,
-  dorsal smallint,
-  position player_position not null
-);
+create table
+  players (
+    id integer not null primary key,
+    first_name character(30) not null unique,
+    last_name character(30),
+    birthdate date not null,
+    height smallint not null,
+    weight smallint not null,
+    dorsal smallint,
+    position player_position not null
+  );
