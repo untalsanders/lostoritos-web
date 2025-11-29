1d5173cc0ab2b277f730d95f21b8c19ce63484ec
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-11-29 05:10:18 -0300

    refactor(config): remove unused platform-browser import

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http'
 import { ApplicationConfig } from '@angular/core'
-import {} from '@angular/platform-browser'
 import { provideRouter } from '@angular/router'
 import { provideAngularSvgIcon } from 'angular-svg-icon'
 import { routes } from './app.routes'
